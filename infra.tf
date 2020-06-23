cf2150b6f52743def8d922ba1bc3ff12afef2fde
Woclandiner Clapp <woclandiner@gmail.com>
2020-06-22 22:36:16 -0400

    Terraform AWS Setup - CHG0004a

--- infra.tf
+++ infra.tf
@@ -167,3 +167,98 @@ resource "aws_route_table" "TRF_RT_INTERNAL" {
         Origin = "TRF"
     }
 }
+
+
+### CREATE ROUTE TO INTERNET
+
+resource "aws_route" "TRF_Internet_Route" {
+
+    route_table_id = aws_route_table.TRF_RT_PUBLIC.id
+    destination_cidr_block = var.destinationdefault
+    gateway_id = aws_internet_gateway.TRF_IGW.id
+
+}
+
+### CREATE ELASTIC IP
+
+resource "aws_eip" "TRF_EIP"{
+
+    tags = {
+        Name = "TRF_EIP"
+        Origin = "TRF"
+    }
+
+}
+
+### CREATE NAT GATEWAY
+
+resource "aws_nat_gateway" "TRF_NAT_GW" {
+    subnet_id = aws_subnet.TRF_Ext_Sub.id
+    allocation_id = aws_eip.TRF_EIP.id
+
+    tags = {
+        Name = "TRF_NAT_GW"
+        Origin = "TRF"
+    }
+
+}
+
+### CREATE ROUTE TO NAT GATEWAY FROM INTERNAL NETWORK
+
+resource "aws_route" "TRF_Nat_Route" {
+
+    route_table_id = aws_route_table.TRF_RT_INTERNAL.id
+    destination_cidr_block = var.destinationdefault
+    gateway_id = aws_nat_gateway.TRF_NAT_GW.id
+
+}
+
+### ASSOCIATE NETWORKS TO ROUTE TABLE
+
+resource "aws_route_table_association" "TRF_vpc_association01" {
+
+    subnet_id      = aws_subnet.TRF_Ext_Sub.id
+    route_table_id = aws_route_table.TRF_RT_PUBLIC.id
+
+}
+
+resource "aws_route_table_association" "TRF_vpc_association02" {
+
+    subnet_id      = aws_subnet.TRF_Int_Sub.id
+    route_table_id = aws_route_table.TRF_RT_INTERNAL.id
+
+}
+
+### CREATE WEBSERVER INSTANCE
+
+resource "aws_instance" "WebServer_TRF" {
+
+    ami = "ami-2757f631"
+    instance_type = "t2.micro"
+    associate_public_ip_address = true
+    subnet_id = aws_subnet.TRF_Ext_Sub.id
+    vpc_security_group_ids = [aws_security_group.TRF_SG_WEB.id]
+
+    tags = {
+        Name = "WebServer"
+        Origin = "TRF"
+    }
+
+}
+
+### CREATE DATABASE INSTANCE
+
+resource "aws_instance" "Database_TRF" {
+
+    ami = "ami-2757f631"
+    instance_type = "t2.micro"
+    associate_public_ip_address = false
+    subnet_id = aws_subnet.TRF_Int_Sub.id
+    vpc_security_group_ids = [aws_security_group.TRF_SG_DB.id]
+
+    tags = {
+        Name = "Database"
+        Origin = "TRF"
+    }
+
+}
